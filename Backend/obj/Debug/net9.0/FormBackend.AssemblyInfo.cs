9d64de3853c5ec1891bc136c9e60e14ea2cadb6f
madanpandey47 <zeusmadan97@gmail.com>
2025-12-22 14:50:08 +0545

    ...

--- Backend/obj/Debug/net9.0/FormBackend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/FormBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fc1a63ac706470ada0a246e1ca82f50bc595ec5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+377539debdb46f7104ff013c889115762f8580f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
